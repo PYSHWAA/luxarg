8eabb611ebe0ca67ef49516673b9e0f85620b830
amzy-0 <azymiamin@gmail.com>
2021-07-13 00:37:14 +0430

    unsed import removed

--- libs/read_write.py
+++ libs/read_write.py
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from tkinter import messagebox
-from os import path, replace
+from os import path
 from os import getenv
 
 
