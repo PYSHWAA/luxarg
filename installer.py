d20b438d85229a16e70f4b71283a3f9ff543b439
amzy-0 <azymiamin@gmail.com>
2021-08-21 09:41:04 +0430

    all the dependancies have to install with the root permission

--- installer.py
+++ installer.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 from os import system, getenv
-
+system('sudo pip install -r requirements.txt')
 
 # quit from installed message
 def quit(key):
