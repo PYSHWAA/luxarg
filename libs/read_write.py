345fafda8ea3e4252ef55eb63d2faf39994ea989
amzy-0 <azymaimin@gmail.com>
2021-09-25 19:57:03 +0330

    Posix compatible mode (feature) added for (MS-DOS) & Windows.

--- libs/read_write.py
+++ libs/read_write.py
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from tkinter import messagebox
 from os import path
-from os import getenv
 
 def message(path_msg, text_msg):
 
@@ -36,55 +35,6 @@ def message(path_msg, text_msg):
         message='%s  %s' % (path_msg, text_msg))
 
 
-# def writer(path_and_filename, text, widget_destroy):
-    
-#     # path_and_filename equal to EMPTY 
-#     if path_and_filename == '':
-#         path_and_filename = '''Field is empty !
-# Please HIT <F2> and enter your file name again ...'''
-#         message(path_and_filename, '')
-
-#     # if is directory :
-#     elif path.isdir(path_and_filename) or path_and_filename[-1] == '/':
-#         # if not a file 
-#         message(path_and_filename, ': Is directory')   
-
-#     elif path_and_filename[:2] == '~/':
-#         path_and_filename = path_and_filename.replace('~/','%s/'% 
-#         str(getenv('HOME'))).strip()
-    
-#         try:
-#             fin = open(path_and_filename, 'w')
-#             fin.write(text)
-#             message(path_and_filename, 'saved !')
-            
-#             fin.close()
-        
-#         except OSError as error :
-#             message(path_and_filename, str(error)[10:])
-    
-
-
-    
-#     # relational path for home directory added (~)  
-#     elif path_and_filename == '~':
-#             message(path_and_filename, ': Is directory')
-
-#     else:
-        
-#         try:
-#             fin = open(path_and_filename, 'w')
-#             fin.write(text)
-#             message(path_and_filename, 'saved !')
-            
-#             fin.close()
-        
-#         except OSError as error :
-#             message(path_and_filename, str(error)[10:])
-    
-
-#     return widget_destroy.destroy()
-
 ####################################################
 # IO 
 ####################################################
@@ -95,7 +45,7 @@ def io_luxarg(path_and_filename, text, io_mode, text_field, file_path):
     if path_and_filename[:2]=='~/':
         
         path_and_filename = path_and_filename.replace('~/', '%s/' % 
-        str(getenv('HOME').strip()))
+        str(path.expanduser('~').strip()))
         
     # path_and_filename equal to EMPTY 
     if path_and_filename == '':
@@ -138,7 +88,7 @@ def io_luxarg(path_and_filename, text, io_mode, text_field, file_path):
     # relational path for home
     elif io_mode=='w':
         path_and_filename = path_and_filename.replace('~/','%s/'% 
-        str(getenv('HOME'))).strip()
+        str(path.expanduser('~'))).strip()
         
         try:
             fin = open(path_and_filename, io_mode)
