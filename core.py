fd14e316f604e38d24897c766125ecdb0f1d98eb
amzy-0 <azymiamin@gmail.com>
2021-09-26 17:43:46 +0330

    getenv function added (absolute path)

--- core.py
+++ core.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
 
-from os import path
+from os import path, getenv
 from sys import argv, exit
 from tkinter import BOTH, RIGHT, SUNKEN, Tk, Text, Scrollbar,Label, Entry
 from libs.keys_actions import *
