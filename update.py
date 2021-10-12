172e5ac50d5bd933afa0eddb510ec460213f9912
amzy-0 <azymiamin@gmail.com>
2021-10-12 18:05:20 +0330

    LICENSE updated

--- update.py
+++ update.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
-'''
 
+''' 
 AMZY-0 (M.Amin Azimi .K) 
-Copyright (C) (2019-2020-2021)  AMZY-0 (M.Amin Azimi .K) 
+Copyright (C) 2019-2021 luxarg AMZY-0 (M.Amin Azimi .K) and contributors
 
-"Luxarg" (This program) is free software: you can redistribute it and/or modify
+Luxarg is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
+Luxarg is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This file is part of Luxarg.
+Luxarg is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Luxarg is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
 
 '''
 
@@ -25,10 +36,8 @@ chdir('%s/.luxarg/' % getenv('HOME'))
 
 system('git reset FETCH_HEAD; git restore . ; git pull')
 
-system('sudo rm -f /usr/bin/luxarg')
-system('rm -f ./luxarg; ./installer.py')
-system('sudo cp -f luxarg /usr/bin/')
-system('rm luxarg')
+system('make install clean')
+
 print('...')
 
 
